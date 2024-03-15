16e50275c50395683dd97502a4e14d471beb5ae4
riken-khadela <rikenkhadelamain@gmail.com>
2024-03-15 11:26:27 +0530

    sdsd

--- check_views.py
+++ check_views.py
@@ -1,2 +1,5 @@
 from dbb import get_views_per_day
-print(get_views_per_day)
+views_per_day = get_views_per_day()
+body = ''
+for date, count in views_per_day.items():
+    print(f"Date: {date}, Total Views: {count}")
