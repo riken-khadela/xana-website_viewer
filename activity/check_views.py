f1a4181c0ebe3a93588ab4b32c68fd17b6fb8fd2
riken-khadela <rikenkhadelamain@gmail.com>
2024-04-28 18:20:29 +0530

    added check views

---
+++ activity/check_views.py
@@ -0,0 +1,8 @@
+from dbb import get_views_per_day, add_data_with_view, create_db_ifnot, get_today_views
+from datetime import datetime
+create_db_ifnot()
+add_data_with_view(datetime.now().strftime("%d/%m/%Y"), True)
+views_per_day = get_views_per_day()
+body = ''
+for date, count in views_per_day.items():
+    print(f"Date: {date}, Total Views: {count}")
