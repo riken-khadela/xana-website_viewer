c68778d1661ffa7c0ad6642cc6a270cc8b1e7d35
riken-khadela <rikenkhadelamain@gmail.com>
2024-05-14 16:24:18 +0530

    dssd

--- bot.sh
+++ bot.sh
@@ -1,15 +1,19 @@
-
 # project path
 export CURRENT_DIR=`dirname $(readlink -f $0)`
 export PRJ_DIR=`dirname $CURRENT_DIR`
 # go to project root directory
-
 cd $PRJ_DIR
-pwd
-killall -9 python qemu-system-x86_64
-cd /home/rk/Desktop/xana-website_viewer/
-git stash
-git pull 
-. env/bin/activate
-python activity/mbot.py
+# . tasks/environment.sh
 
+# Kill python and AVD process
+killall -9 python qemu-system-x86_64
+
+# activate the virtual environment for python
+#. env/bin/activate
+. env/bin/activate
+
+# update code
+# git checkout old-insta-rk
+git stash
+git pull
+python activity/mbot.py
